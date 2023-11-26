789403f0f41d905533eb19a3252726a6456b9870
Zuardin Akbar <akbar.zuardin@gmail.com>
2023-11-26 23:06:13 +0100

    Initial Commit

--- main.js
+++ main.js
@@ -20,7 +20,11 @@ var ambientLight;
 var directionalLight;
 
 //-- GEOMETRY PARAMETERS
-//Create an empty array for storing all the cubes
+//Create an empty array for storing all the geometrie
+var nodes = [];
+var edges = [];
+var angleMultiplier = 60;
+var level = 10;
  
 
 function main(){
@@ -43,7 +47,11 @@ function main(){
   scene.add(directionalLight.target);
 
   //GEOMETRY INITIATION
-  // Initiate first cubes
+
+  //Testing the Node Class
+ 
+  var location = new THREE.Vector3(0,0,0);
+  generateTree(location, level, 0, null);
 
 
   //RESPONSIVE WINDOW
@@ -66,6 +74,26 @@ function main(){
 //-----------------------------------------------------------------------------------
 //HELPER FUNCTIONS
 //-----------------------------------------------------------------------------------
+
+//RECURSIVE TREE GENERATION
+function generateTree(position, level, parentAngle, parent){
+  var node = new TreeNode(position, level, parentAngle, parent);
+  nodes.push(node);
+
+  if (parent){
+    var edge = new Edge(parent.position, node.position);
+    edges.push(edge);
+  }
+
+  if (level > 0){
+    node.createChildren();
+    for(var i=0; i<node.children.length; i++){
+      var child = node.children[i];
+      generateTree(child.position, child.level, child.angle, node);
+    }
+  }
+}
+
 //RESPONSIVE
 function handleResize() {
   width = window.innerWidth;
@@ -74,6 +102,11 @@ function handleResize() {
   camera.updateProjectionMatrix();
   renderer.setSize(width, height);
   renderer.render(scene, camera);
+}
+
+//RANDOM INTEGER IN A RANGE
+function getRndInteger(min, max){
+  return Math.floor(Math.random() * (max-min+1)) + min;
 }
 
 //ANIMATE AND RENDER
@@ -86,7 +119,75 @@ function animate() {
 //-----------------------------------------------------------------------------------
 // CLASS
 //-----------------------------------------------------------------------------------
+class TreeNode{
+  constructor(position, level, parentAngle, parent){
+    this.position = position;
+    this.level = level;
+    this.parentAngle = parentAngle * (Math.PI/180);
+    this.parentDirection = new THREE.Vector3(0,1,0);
+    this.parent = parent;
+    this.children = [];
 
+    //Calculate the angle based on the parent's angle
+    this.angle = this.parentAngle + getRndInteger(0-angleMultiplier/2, angleMultiplier/2) * (Math.PI/180);
+
+    this.length = this.level === 0 ? 2:Math.random() * 2 + 1;
+
+    //Create the shape for the node
+    var nodeGeometry = new THREE.SphereGeometry(0.1, 10, 10);
+    var material = new THREE.MeshBasicMaterial({color:0x00ff00});
+    this.nodeMesh = new THREE.Mesh(nodeGeometry, material);
+    this.nodeMesh.position.copy(this.position);
+
+    //Add the node to the scene
+    scene.add(this.nodeMesh);
+
+    //Create an edge (branch)connecting to the parent if it exist
+    if (parent){
+      var edge = new Edge(parent.position, this.position);
+      this.edgeMesh = edge.mesh;
+      scene.add(this.edgeMesh);
+    }
+  }
+
+  createChildren(){
+    for(var i=0; i<2; i++){
+      var childPosition = new THREE.Vector3().copy(this.position);
+
+      let axisZ = new THREE.Vector3(0,0,1);
+      let axisY = new THREE.Vector3(0,1,0);
+
+      this.parentDirection.applyAxisAngle(axisZ, this.angle);
+      this.parentDirection.applyAxisAngle(axisY, this.angle);
+
+      childPosition.x += this.parentDirection.x * this.length;
+      childPosition.y += this.parentDirection.y * this.length;
+      childPosition.z += this.parentDirection.z * this.length;
+
+      var child = new TreeNode(childPosition, this.level-1, this.angle, this)
+
+      this.children.push(child);
+      
+    }
+  }
+}
+
+class Edge{
+  constructor(start, end){
+    this.start = start;
+    this.end = end;
+
+    const points = [];
+    points.push(this.start);
+    points.push(this.end);
+
+    var edgeGeometry = new THREE.BufferGeometry().setFromPoints(points);
+
+    var material = new THREE.MeshBasicMaterial({color:0x00ff00});
+
+    this.mesh = new THREE.Line(edgeGeometry, material);
+  }
+}
 
 //-----------------------------------------------------------------------------------
 // EXECUTE MAIN 
